57467a7400372d796d0008871e8d601f2afbedb3
Ted <58144503+BUSH222@users.noreply.github.com>
2024-09-25 19:40:47 +0300

    main structure
    Co-authored-by: nexxy06 <nexxy06@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template('main.html')
+
+
 if __name__ == '__main__':
-    pass  # Do something
+    app.run(host='0.0.0.0', port=8000, debug=True)
