f6b7244b7e2dd68c85b2376b6ed0326a46ab4679
Ted <58144503+BUSH222@users.noreply.github.com>
2024-09-25 22:56:34 +0300

    save in update balance

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ def preload_db():
 def update_balance(suser, updated):
     suser.balance += updated
     cur.execute('UPDATE users SET balance = %s WHERE id = %s', (suser.balance, suser.id))
+    conn.commit()
 
 
 @app.route('/')
